6af523efcc96acc9cdab112af4c8c03ea4a1ddf2
Neethika6 <neethikadakoju346@gmail.com>
2025-06-16 22:03:02 +0530

    ECHO Example

---
+++ 02-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Example of echo statements with some conversation
+
+echo "Trump:: Hey, Hiii Elon"
+echo "Musk:: Hey Trump...Hii"
+echo "Trump:: How are you musk??"
+echo  "Musk:: Not bad..How are you Trump?"
+echo "Trump:: I am good. Thanks for asking! I see that you are struggling with something but not sharing"
+echo "Musk:: You are the TRUE FRIEND Trump!! You understand me without uttering a single word"
