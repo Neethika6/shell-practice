83b6cdc263369019740534ee7fcd446f48d9c97b
Neethika6 <neethikadakoju346@gmail.com>
2025-06-16 22:12:59 +0530

    Variables usage in shell

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+person1="Alia"
+person2="Deepika"
+
+echo "$person1:: Hey, Hiii Elon"
+echo "$person2:: Hey $person1...Hii"
+echo "$person1:: How are you $person2??"
+echo  "$person2:: Not bad..How are you $person1?"
+echo "$person1:: I am good. Thanks for asking! I see that you are struggling with something but not sharing"
+echo "$person2:: You are the TRUE FRIEND $person1!! You understand me without uttering a single word"
+echo "$person1:: Haha:)"
