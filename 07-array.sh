2f39c8dcbff7d020ba8434247c992b58ff206dee
Neethika6 <neethikadakoju346@gmail.com>
2025-06-16 23:23:20 +0530

    variable usage

--- 07-array.sh
+++ 07-array.sh
@@ -2,5 +2,8 @@
 
 a=("apple" "bat" "cat" "180" "324" )
 
-echo "the first value is ${a[0]})"
+echo "the first value is ${a[0]}"
+echo "the last value is ${a[4]}"
+echo "invalid value ${a[5]}"
+echo "all values in array: ${a[@]}"
 
