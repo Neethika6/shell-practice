4af453c2d1b9f8b4fa5e86fb5bc7d3a12c451450
Neethika6 <neethikadakoju346@gmail.com>
2025-06-17 23:44:20 +0530

    SpecialVaraibles

---
+++ 08-specialvariable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed to the script:$@"
+echo "Number of variable passed to the script:$#"
+echo "Name of the scipt:$0"
+echo "Present working directory:$PWD"
+echo "Which user is running the script:$USER"
+echo "Home directory of the user running the script:$HOME"
+echo "PID of the current running script:$$"
+sleep 10 &
+echo "PID of the last command running in the background:$!"
