68d6d0c458d8fed1fb059e1be5190797182d64ef
Neethika6 <neethikadakoju346@gmail.com>
2025-06-17 23:59:54 +0530

    Package Installation

---
+++ 09-installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Installation can be done witht the root
+# id -u this value will be 0 fro root user
+
+id_value=$(id -u)
+if [ $id_value -eq 0 ]
+then
+    echo "You are in ROOT..You can proceed with installation"
+else
+    echo "Please switch to ROOT"
+fi
