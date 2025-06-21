508097262fdca8f8777ab75e6ef913e9fd57981e
Neethika6 <neethikadakoju346@gmail.com>
2025-06-21 15:23:41 +0530

    Installtion using FOR loop

---
+++ 11-installation_forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Check if you are running the script as a ROOT User or not
+
+user_id=$(id -u)
+
+if ( $user_id -eq 0 )
+then
+    echo "You are running the script as ROOT"
+else
+    echo "ERROR:: You are running the script as NON-ROOT.\nPlease Switch to ROOT"
+fi
