8dff070a46629b38ec6599ec001fa24287e23206
Neethika6 <neethikadakoju346@gmail.com>
2025-06-21 16:39:32 +0530

    Installation using FOR LOOP

--- 11-installation_forloop.sh
+++ 11-installation_forloop.sh
@@ -34,11 +34,11 @@ INSTALLATION_VALIDATION()
 
 for package in ${PACKAGE_ARRAY[@]}
 do
-dnf list installed $package -y #Check of the nginx is already installed or not
+dnf list installed $package #Check of the nginx is already installed or not
     if [ $? -ne 0 ] #$? values returns 0 if the statement which was last executed is success if not it will return 1 to 127
     then
         echo -e "$Y Proceed with $package installation"
-        dnf install $package
+        dnf install $package -y
         INSTALLATION_VALIDATION $? "$package"
     else
         echo -e "$Y $package is already installed"
