049b52b42b9653f62a66286e8685a0d3c0ee5143
Neethika6 <neethikadakoju346@gmail.com>
2025-07-05 17:36:02 +0530

    diskmonitor

--- 15-diskspace.sh
+++ 15-diskspace.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -x
-threshold=1
+threshold=10
 while read i
 do
     disk_usage=$(df -h | awk '$6 == $i {print $5}' | cut -d "%" -f1)
-    if [ $disk_usage >= 1 ] 
+    if [ $disk_usage >= $threshold ] 
     then
         echo "$i is $disk_uage"
     fi
