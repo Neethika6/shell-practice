3943b71bda0d7fc80a237c76a94bc5b8971ffc01
Neethika6 <neethikadakoju346@gmail.com>
2025-07-08 20:55:42 +0530

    diskspace

--- 15-diskspace.sh
+++ 15-diskspace.sh
@@ -4,7 +4,7 @@ while read i
 do
     disk_usage=$(df -h | grep $i | awk '{print $5}'| cut -d "%" -f1 | awk NR==1)
     echo $disk_usage
-    if [ $disk_usage >= $threshold ]  
+    if [ $disk_usage -ge $threshold ]  
     then
         echo "$i is $disk_usage"
     fi
